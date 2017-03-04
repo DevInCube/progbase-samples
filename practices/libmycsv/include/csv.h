f6d6dec0a62e28519b52081f494b94b14ae827cf
PublicHadyniak <public.hadyniak@gmail.com>
2017-03-04 12:36:14 +0200

    more interface functions

--- practices/libmycsv/include/csv.h
+++ practices/libmycsv/include/csv.h
@@ -1,13 +1,29 @@
 #ifndef MODULE_H
 #define MODULE_H
 
+#include <stdbool.h>
+
 // paste your module interface here
 
 typedef struct CsvRow CsvRow;
 typedef struct CsvDoc CsvDoc;
 
+CsvRow * CsvRow_new(void);
+
 CsvDoc * CsvDoc_newFromFile(const char * filePath);
 void CsvDoc_free(CsvDoc ** selfPtr);
-void CsvDoc_writeToFile(const char * filePath);
+void CsvDoc_writeToFile(CsvDoc * self, const char * filePath);
+
+CsvRow * CsvDoc_getRow(CsvDoc * self);
+bool CsvDoc_moveNext(CsvDoc * self);
+void CsvDoc_reset(CsvDoc * self);
+
+void CsvDoc_addRow(CsvDoc * self, CsvRow * row);
+
+const char * CsvRow_getRow(CsvRow * self);
+bool CsvRow_moveNext(CsvRow * self);
+void CsvRow_reset(CsvRow * self);
+
+void CsvRow_addValue(CsvRow * self, const char * value);
 
 #endif
--- practices/libmycsv/src/csv.c
+++ practices/libmycsv/src/csv.c
@@ -6,6 +6,10 @@
 // typedef struct CsvRow CsvRow;
 // typedef struct CsvDoc CsvDoc;
 
+CsvRow * CsvRow_new(void) {
+	return NULL;
+}
+
 CsvDoc * CsvDoc_newFromFile(const char * filePath) {
 	return NULL;
 }
@@ -14,6 +18,19 @@ void CsvDoc_free(CsvDoc ** selfPtr) {
 
 }
 
-void CsvDoc_writeToFile(const char * filePath) {
+void CsvDoc_writeToFile(CsvDoc * self, const char * filePath) {
 
 }
+
+
+CsvRow * CsvDoc_getRow(CsvDoc * self) { return NULL; }
+bool CsvDoc_moveNext(CsvDoc * self) {return false;}
+void CsvDoc_reset(CsvDoc * self) {}
+
+void CsvDoc_addRow(CsvDoc * self, CsvRow * row){}
+
+const char * CsvRow_getRow(CsvRow * self){return NULL;}
+bool CsvRow_moveNext(CsvRow * self){return false;}
+void CsvRow_reset(CsvRow * self){}
+
+void CsvRow_addValue(CsvRow * self, const char * value){}
