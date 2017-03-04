3d8352bd214684ce09358b957a0b61271f9982af
PublicHadyniak <public.hadyniak@gmail.com>
2017-03-04 14:19:57 +0200

    csv refactor

--- practices/libmycsv/main.c
+++ practices/libmycsv/main.c
@@ -3,18 +3,27 @@
 
 #include <csv.h>
 
+void CsvDoc_print(CsvDoc * doc);
+
 int main(void) {
 	CsvDoc * doc = CsvDoc_newFromFile("students.csv");
-	while(CsvDoc_moveNext(doc)) {
-		CsvRow * row = CsvDoc_getRow(doc);
-		while(CsvRow_moveNext(row)) {
-			const char * value = CsvRow_getValue(row);
-			printf("%s, ", value);
-		}
-		puts("");
-	}
-	puts("END");
+	CsvDoc_print(doc);
 	CsvDoc_writeToFile(doc, "out.csv");
 	CsvDoc_free(&doc);
 	return 0;
+}
+
+void CsvDoc_print(CsvDoc * doc) {
+	CsvDoc_reset(doc);
+	while(CsvDoc_moveNext(doc)) {
+		CsvRow * row = CsvDoc_getRow(doc);
+		CsvRow_reset(row);
+		int i = 0;
+		while(CsvRow_moveNext(row)) {
+			if (i++ != 0) printf(", ");
+			const char * value = CsvRow_getValue(row);
+			printf("%s", value);
+		}
+		puts("");
+	}
 }
