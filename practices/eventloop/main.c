5401da73ebc6388c2e38394f886d98bceaac2c51
PublicHadyniak <public.hadyniak@gmail.com>
2017-03-06 00:31:09 +0200

    some generalization

--- practices/eventloop/main.c
+++ practices/eventloop/main.c
@@ -5,6 +5,8 @@
 #include <progbase.h>
 #include <pbconsole.h>
 
+typedef void (*EventHandler)(Event * event, EventQueue * events);
+
 typedef struct KeyInputEvent KeyInputEvent;
 struct KeyInputEvent {
 	struct { Event; };  // -Wno-microsoft to disable this error
@@ -40,29 +42,40 @@ void RandomNumberEvent_free(RandomNumberEvent ** selfPtr) {
 	*selfPtr = NULL;
 }
 
-void RandomEventGen_update(EventQueue * events);
-void InputManager_update(EventQueue * events);
-
+void RandomEventGen_update(Event * event, EventQueue * events);
+void InputManager_update(Event * event, EventQueue * events);
 void Event_handleEvent(Event * event, EventQueue * events);
+
+enum { UpdateEventTypeId = 0 };
 
 int main(void) {
 	srand(time(NULL));
+
+	EventHandler handlers[] = {
+		RandomEventGen_update,
+		InputManager_update,
+		Event_handleEvent
+	};
+	int handlersLength = sizeof(handlers) / sizeof(handlers[0]);
+	
 	EventQueue * eventList = EventQueue_new();
 	while (1) {
-		RandomEventGen_update(eventList);
-		InputManager_update(eventList);
+		Event * updateEvent = Event_new(UpdateEventTypeId, NULL);
+		EventQueue_enqueue(eventList, updateEvent);
 		while (EventQueue_size(eventList) > 0) {
 			Event * event = EventQueue_dequeue(eventList);
-			Event_handleEvent(event, eventList);
+			for (int i = 0; i < handlersLength; i++) {
+				handlers[i](event, eventList);
+			}
 			Event_free(&event);
 		}
-		sleepMillis(33);
+		sleepMillis(33);  // ~ 30 FPS
 	}
 	EventQueue_free(&eventList);
 	return 0;
 }
 
-void RandomEventGen_update(EventQueue * events) {
+void RandomEventGen_update(Event * event, EventQueue * events) {
 	if (rand() % 33 == 0) {
 		int number = rand() % 200 - 100;
 		EventQueue_enqueue(
@@ -71,7 +84,7 @@ void RandomEventGen_update(EventQueue * events) {
 	}
 }
 
-void InputManager_update(EventQueue * events) {
+void InputManager_update(Event * event, EventQueue * events) {
 	if (conIsKeyDown()) {
 		char ch = getchar();
 		EventQueue_enqueue(
@@ -84,6 +97,10 @@ enum { CustomEventTypeId = 124090 };
 
 void Event_handleEvent(Event * event, EventQueue * eventList) {
 	switch (event->type) {
+		case UpdateEventTypeId: {
+			putchar('.');
+			break;
+		}
 		case RandomNumberEventTypeId: {
 			RandomNumberEvent * randEvent = (RandomNumberEvent *)event;
 			printf("Random number %i\n", randEvent->number);
