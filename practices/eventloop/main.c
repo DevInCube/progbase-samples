0c88c0a2a421d4a038bf7c4c28654fbdf42269ee
PublicHadyniak <public.hadyniak@gmail.com>
2017-03-06 23:54:09 +0200

    Exit event and memory leak fix

--- practices/eventloop/main.c
+++ practices/eventloop/main.c
@@ -15,7 +15,7 @@ enum { KeyInputEventTypeId = 47578 };
 Event * KeyInputEvent_new(void * sender, char key) {
 	KeyInputEvent * data = malloc(sizeof(struct KeyInputEvent));
 	data->keyCode = key;
-	return Event_new(sender, KeyInputEventTypeId, data, NULL);
+	return Event_new(sender, KeyInputEventTypeId, data, free);
 }
 
 typedef struct RandomNumberEvent RandomNumberEvent;
@@ -36,6 +36,7 @@ void CustomHandler_handleEvent(void * self, Event * event);
 void Timer_handleEvent(void * self, Event * event);
 
 enum { 
+	ExitEventTypeId = -1,
 	UpdateEventTypeId = 0,
 	StartEventTypeId = 1,
 	RemoveHandlerEventTypeId = 767456
@@ -87,15 +88,16 @@ int main(void) {
 
 	List_add(g_eventSystem->handlers, HandlerObject_new(NULL, NULL, RandomEventGen_update));
 	List_add(g_eventSystem->handlers, HandlerObject_new(NULL, NULL, InputManager_update));
-	int counter = 0;
-	List_add(g_eventSystem->handlers, HandlerObject_new(&counter, NULL, CustomHandler_handleEvent));
+	int spaceHitCounter = 0;
+	List_add(g_eventSystem->handlers, HandlerObject_new(&spaceHitCounter, NULL, CustomHandler_handleEvent));
 	int timeCounter = 100;
 	List_add(g_eventSystem->handlers, HandlerObject_new(&timeCounter, NULL, Timer_handleEvent));
 
 	EventQueue_enqueue(
 		g_eventSystem->events, 
 		Event_new(NULL, StartEventTypeId, NULL, NULL));
-	while (1) {
+	bool isRunning = true;
+	while (isRunning) {
 		EventQueue_enqueue(
 			g_eventSystem->events, 
 			Event_new(NULL, UpdateEventTypeId, NULL, NULL));
@@ -105,12 +107,19 @@ int main(void) {
 				HandlerObject * handler = List_get(g_eventSystem->handlers, i);
 				handler->handler(handler->self, event);
 			}
+			if (event->type == ExitEventTypeId) {
+				isRunning = false;
+			}
 			if (event->type == RemoveHandlerEventTypeId) {
 				__removeHandlerWithData(event->data);
 			}
 			Event_free(&event);
 		}
 		sleepMillis(33);  // ~ 30 FPS
+	}
+	while (EventQueue_size(g_eventSystem->events) > 0) {
+		Event * event = EventQueue_dequeue(g_eventSystem->events);
+		Event_free(&event);
 	}
 	EventQueue_free(&g_eventSystem->events);
 	for (int i = 0; i < List_count(g_eventSystem->handlers); i++) {
@@ -154,18 +163,14 @@ void RandomEventGen_update(void * self, Event * event) {
 	}
 	if (rand() % 33 == 0) {
 		int number = rand() % 200 - 100;
-		EventQueue_enqueue(
-			g_eventSystem->events, 
-			RandomNumberEvent_new(self, number));
+		EventSystem_raiseEvent(RandomNumberEvent_new(self, number));
 	}
 }
 
 void InputManager_update(void * self, Event * event) {
 	if (conIsKeyDown()) {
 		char keyCode = getchar();
-		EventQueue_enqueue(
-			g_eventSystem->events, 
-			KeyInputEvent_new(self, keyCode));
+		EventSystem_raiseEvent(KeyInputEvent_new(self, keyCode));
 	}
 }
 
@@ -189,9 +194,7 @@ void CustomHandler_handleEvent(void * self, Event * event) {
 		case KeyInputEventTypeId: {
 			KeyInputEvent * keyEvent = (KeyInputEvent *)event->data;
 			if (keyEvent->keyCode == ' ') {
-				EventQueue_enqueue(
-					g_eventSystem->events, 
-					Event_new(self, CustomEventTypeId, NULL, NULL));
+				EventSystem_raiseEvent(Event_new(self, CustomEventTypeId, NULL, NULL));
 			}
 			if (keyEvent->keyCode == 'a') {
 				int * timer = malloc(sizeof(int));
@@ -201,6 +204,9 @@ void CustomHandler_handleEvent(void * self, Event * event) {
 				handler->handler = Timer_handleEvent;
 				handler->destructor = free;
 				EventSystem_addHandler(handler);
+			}
+			if (keyEvent->keyCode == 'q') {
+				EventSystem_raiseEvent(Event_new(self, ExitEventTypeId, NULL, NULL));
 			}
 			printf("Key pressed `%c` [%i]\n", 
 				keyEvent->keyCode, keyEvent->keyCode);
