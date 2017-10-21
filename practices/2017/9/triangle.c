112fe71ca31cdcc2ccba2ec25135d06ddb298142
Ruslan Hadyniak <public.hadyniak@gmail.com>
2017-10-21 11:48:25 +0300

    Create triangle.c

---
+++ practices/2017/9/triangle.c
@@ -0,0 +1,45 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <assert.h>
+
+enum TriangleType
+{
+    NEISNUYE,
+    RIVNOSTORONNIY,
+    RIVNOBEDRENNIY,
+    DOVILNIY
+};
+
+enum TriangleType getTriangleType(int a, int b, int c);
+void printTriangleType(enum TriangleType type);
+
+int main(void)
+{
+    assert(getTriangleType(3, 4, 5) == DOVILNIY);
+    assert(getTriangleType(3, 3, 3) == RIVNOSTORONNIY);
+    assert(getTriangleType(3, 3, 4) == RIVNOBEDRENNIY);
+    assert(getTriangleType(1, 1, 10) == NEISNUYE);
+    //
+    enum TriangleType t = getTriangleType(3, 4, 5);
+    printTriangleType(t);
+    return 0;
+}
+
+enum TriangleType getTriangleType(int a, int b, int c)
+{
+    return NEISNUYE;
+}
+
+void printTriangleType(enum TriangleType type)
+{
+    switch (type)
+    {
+    case NEISNUYE:
+        printf("Ne isnuye\n");
+        break;
+    case RIVNOSTORONNIY:
+        printf("Rivnostoronniy\n");
+        break;
+        //
+    }
+}
